51fc8fc30ce53f544285a333c4b2f94d95bab0f5
Manuel Arnauda <143724150+manuelarnauda@users.noreply.github.com>
2023-09-05 09:37:18 -0500

    Add files via upload

---
+++ Tareas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tareasprogramacion;
+
+/**
+ *
+ * @author labcca
+ */
+public class TareasProgramacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int tareas;
+        String tarea1 = "calculo";
+        String tarea2 = "fundamentos de la investigacion";
+        
+        System.out.println("El primer trabajo a realizar es de " + tarea1 + " mientras que el segundo a realizar es de " + tarea2);
+    }
+    
+}
