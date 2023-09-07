628d37355ca0a7b248393ab4d742ac3fd89e1ca0
Manuel Arnauda <143724150+manuelarnauda@users.noreply.github.com>
2023-09-07 08:44:52 -0600

    Add files via upload

---
+++ NombreAreaRectangulo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author labcca
+ */
+public class NombreAreaRectangulo {
+    public static void main(String[] args) {
+        
+        int nombre;
+        
+        System.out.println("Mi nombre es Victor Manuel Arnauda Ramirez ");
+        
+        int base = 45;
+        int altura = 4;
+        
+        int areaRectangulo = base * altura;
+        
+        double baseA = 5.8;
+        double alturaB = 4.2;
+        
+        double areaRectanguloDecimal = baseA * alturaB;
+        
+        System.out.println("El area del rectangulo es de: " + areaRectangulo);
+        System.out.println("El area del rectangulo con decimales es: " + areaRectanguloDecimal);
+    }
+    
+}
