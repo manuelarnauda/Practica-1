42d3e4e06ccbd07f71b9d24373dec52d1d907246
Manuel Arnauda <143724150+manuelarnauda@users.noreply.github.com>
2023-09-14 07:41:19 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libreriascanner;
+import java.util.Scanner;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaTareas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Esta es mi lista de tareas: ");
+        String tarea1 = scanner.nextLine();
+        System.out.println("La primera tarea es de etica");
+        String tarea2 = scanner.nextLine();
+        System.out.println("La segunda tarea es de investigacion");
+        String tarea3 = scanner.nextLine();
+        System.out.println("La tercera tarea es de calculo");
+        scanner.close();
+        
+        
+        
+        
+    }
+    
+}
